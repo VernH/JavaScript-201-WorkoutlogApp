d665ce084ebd1b5dea20e2c8e4cb387e01fa85dd
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-26 14:29:27 -0500

    module-7-sequelize code added

--- server/app.js
+++ server/app.js
@@ -13,7 +13,55 @@ app.listen(3000, function(){
 });
 
 
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'Letmein1234!', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
 
+sequelize.authenticate().then(
+	function() {
+		console.log('connected to workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+
+var User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
+});
+
+
+User.sync();
+
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req, res) {
+		var username = req.body.user.username;
+		var pass = req.body.user.password;
+		//Need to create a user object and use sequelize to put that user into
+		//
+
+		User.create({
+			username: username,
+			passwordhash: ""
+		}).then(
+		//Sequelize is going to return the object it created from db.
+
+			function createSuccess(user){
+				res.json({
+						user: user,
+						message: 'create'
+				});
+			},
+			function createError(err){
+				res.send(500, err.message);
+			}
+		);
+	});
 
 
 
