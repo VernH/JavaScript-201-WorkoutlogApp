fdd95e85b03486dac8d6b5aa811ff0b571f08f8c
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-26 14:23:07 -0500

    module-5-middleware

--- server/app.js
+++ server/app.js
@@ -4,13 +4,15 @@ var bodyParser = require('body-parser');
 
 app.use(require('./middleware/headers'));
 
+app.use('/api/test', function(req, res){
+	res.send("Hello World");
+});
+
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
 });
 
-app.use('/api/test', function(req, res){
-	res.send("Hello World");
-});
+
 
 
 
