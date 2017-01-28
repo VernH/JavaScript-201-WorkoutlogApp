6c0f0b398fd81d78ca4b791b25c01687eb3c6184
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-27 20:02:05 -0500

    module-12-jwt

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
-var User = sequelize.import(__dirname + '\\models\\user');
+
+var User = sequelize.import('./models/user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
-
-
-//sequelize.sync(); //To reset the table sync({ force: true })
 
 app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
 
 
 app.use('/api/user', require('./routes/user'));
@@ -27,8 +28,6 @@ app.listen(3000, function(){
 	console.log('App is listening on 3000.')
 });
 
-
-app.use(bodyParser.json());
 
 
 
