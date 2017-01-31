03ca4b98a4777e31a9b02ce7212c8d39d351ed34
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-31 07:19:04 -0500

    mod 10 update/cleanup

--- server/app.js
+++ server/app.js
@@ -2,20 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
+
+
 var User = sequelize.import(__dirname + '\\models\\user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
-
-//sequelize.sync(); //To reset the table sync({ force: true })
-
 app.use(bodyParser.json());
-
 app.use(require('./middleware/headers'));
-
-
 app.use('/api/user', require('./routes/user'));
-
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
@@ -23,9 +18,6 @@ app.use('/api/test', function(req, res){
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
 });
-
-
-app.use(bodyParser.json());
 
 
 
