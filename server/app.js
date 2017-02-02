97c1fbecdb59e234b70704de940c1fed201544b2
Paul O'Connor <jamespauloconnor@gmail.com>
2017-02-02 07:35:03 -0500

    A few updates for the video

--- server/app.js
+++ server/app.js
@@ -13,11 +13,18 @@ app.listen(3000, function(){
 });
 
 
+
+
+
+
+
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('workoutlog', 'postgres', 'Letmein1234!', {
 	host: 'localhost',
 	dialect: 'postgres'
 });
+
+
 
 sequelize.authenticate().then(
 	function() {
@@ -29,36 +36,36 @@ sequelize.authenticate().then(
 );
 
 
+//Data Model
 var User = sequelize.define('user', {
 	username: Sequelize.STRING,
 	passwordhash: Sequelize.STRING,
 });
 
-
 User.sync();
-
 app.use(bodyParser.json());
 
 app.post('/api/user', function(req, res) {
-		var username = req.body.user.username;
+		var user = req.body.user.username;
 		var pass = req.body.user.password;
 		//Need to create a user object and use sequelize to put that user into
 		//
 
 		User.create({
-			username: username,
-			passwordhash: ""
+			username: user,
+			passwordhash: pass
 		}).then(
 		//Sequelize is going to return the object it created from db.
 
 			function createSuccess(user){
 				res.json({
 						user: user,
-						message: 'create'
+						message: 'you did it!!!'
 				});
 			},
 			function createError(err){
 				res.send(500, err.message);
+
 			}
 		);
 	});
